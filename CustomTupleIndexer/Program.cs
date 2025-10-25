35de670b1683faa5e7a93a66ba008b5b78944011
giftos1 <giftsydney676@gmail.com>
2025-10-25 18:07:57 +0800

    Initial Commit

---
+++ CustomTupleIndexer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomTupleIndexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
