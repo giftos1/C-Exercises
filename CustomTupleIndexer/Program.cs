f058c18faaeb7a603867e5740c35e5afaa0fa7ed
giftos1 <giftsydney676@gmail.com>
2025-10-26 20:22:47 +0800

    add functionality implementing CustomTuple class

--- CustomTupleIndexer/Program.cs
+++ CustomTupleIndexer/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            var fruits = new string[] { "Apples", "Mangoes", "Bananas", "Grapes", "Rambutan" };
+
+            var pairofArrays = new PairOfArrays<int, string>(numbers, fruits);
+
+            //Console.WriteLine(pairofArrays[10, 2]); // out of range
+            //Console.WriteLine(pairofArrays[2, 2]); // (3, Grapes)
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < fruits.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.WriteLine($"Before: Indexes ({i}, {j}) => Values {pairofArrays[i, j]}");
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
         }
     }
 }
